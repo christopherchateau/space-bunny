80ae4d3325b6273482b6ca5ea3234701d3bf8669
Chris Chateau <christopherchateau@gmail.com>
2018-08-15 22:23:39 -0600

    Rewrite movement test for GamePiece

--- test/Game-test.js
+++ test/Game-test.js
@@ -3,27 +3,35 @@ const Game = require('../lib/Game');
 
 const ctx = {
   canvas: {
-    width: 300,
-    height: 300
+    width: 700,
+    height: 700
   }
 }
 
 describe('Game', () => {
+  let game;
 
-  it.skip('should end game if block collides with wall', () => {
-
-    const game = new Game(ctx);
-    const block = game.blocks[0];
-
-    // block.x = ctx.canvas.width;
-
-    game.handleBlock(block);
-
-    assert.isTrue(game.gameOver());
+  beforeEach( () => {
+    game = new Game(ctx);
   });
 
-  it.skip('should take properties', () => {})
+  // it.skip('should end game if block collides with wall', () => {
+
+  //   const game = new Game(ctx);
+  //   const block = game.blocks[0];
+  //   block.x = ctx.canvas.width;
+  //   game.handleBlock(block);
+  //   assert.isTrue(game.gameOver());
+  // });
+
+  it('should take properties', () => {
+    // console.log(game)
+    // assert.deepEqual(gamepiece, {
+    //   x: 50,
+
+    // })
+  })
+
   it.skip('should end game', () => {})
-  it.skip('should collide with walls', () => {})
-  it.skip('should be able to changeDirection', () => {})
+
 })
